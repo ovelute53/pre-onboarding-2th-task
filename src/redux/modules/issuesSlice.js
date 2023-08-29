b7a530c782ce319ce083ac2c8e53f99ec3f65448
ovelute53 <ovelute53@naver.com>
2023-08-29 22:13:20 +0900

    feat: redux slice created

---
+++ src/redux/modules/issuesSlice.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import api from './api';
+
+export const fetchIssues = createAsyncThunk('issues/fetchIssues', async () => {
+  const response = await api.get('/repos/facebook/react/issues?state=open');
+  return response.data;
+});
+
+export const fetchIssueDetail = createAsyncThunk('issues/fetchIssueDetail', async issueNumber => {
+  const response = await api.get(`/repos/facebook/react/issues/${issueNumber}`);
+  return response.data;
+});
+
+const issuesSlice = createSlice({
+  name: 'issues',
+  initialState: {
+    list: [],
+    currentIssue: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(fetchIssues.pending, state => {
+        state.loading = true;
+      })
+      .addCase(fetchIssues.fulfilled, (state, action) => {
+        state.loading = false;
+        state.list = action.payload;
+      })
+      .addCase(fetchIssues.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(fetchIssueDetail.pending, state => {
+        state.loading = true;
+      })
+      .addCase(fetchIssueDetail.fulfilled, (state, action) => {
+        state.loading = false;
+        state.currentIssue = action.payload;
+      })
+      .addCase(fetchIssueDetail.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default issuesSlice.reducer;
