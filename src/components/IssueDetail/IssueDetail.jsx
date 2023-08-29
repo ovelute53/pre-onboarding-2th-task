ebb652d791f90b65bb7ab4b5aa4685fe522c0f8d
ovelute53 <ovelute53@naver.com>
2023-08-29 22:31:53 +0900

    feat: issue 세부사항 생성

---
+++ src/components/IssueDetail/IssueDetail.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchIssueDetail } from '@/redux/modules/issuesSlice';
+
+function IssueDetail() {
+  const { issueNumber } = useParams();
+  const dispatch = useDispatch();
+  const issue = useSelector(state => state.issues.currentIssue);
+  const loading = useSelector(state => state.issues.loading);
+
+  useEffect(() => {
+    dispatch(fetchIssueDetail(issueNumber));
+  }, [dispatch, issueNumber]);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>{issue.title}</h1>
+      <p>{issue.body}</p>
+    </div>
+  );
+}
+
+export default IssueDetail;
