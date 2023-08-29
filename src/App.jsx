1eefcc12e1d8ab1252c7cf98e353d28ad5098eb1
ovelute53 <ovelute53@naver.com>
2023-08-29 22:49:25 +0900

    App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import IssuesList from '@/components/IssuesList/IssuesList';
+import IssueDetail from '@/components/IssueDetail/IssueDetail';
+import GlobalStyles from '@/styles/GlobalStyles';
+import Header from '@/components/Header/Header';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" />
-        <Route path="/" />
+        <GlobalStyles />
+        <Header />
+        <Route path="/" exact Component={IssuesList} />
+        <Route path="/issues/:issueNumber" Component={IssueDetail} />
       </Routes>
     </Router>
   );
