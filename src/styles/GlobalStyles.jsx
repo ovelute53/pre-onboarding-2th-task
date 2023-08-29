09dc6910416ed61d641925f2e90679608ada0191
ovelute53 <ovelute53@naver.com>
2023-08-29 22:46:23 +0900

    styles: GlobalStyle에 reset css 적용

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: Arial, sans-serif;
+  }
+`;
+
+export default GlobalStyles;
