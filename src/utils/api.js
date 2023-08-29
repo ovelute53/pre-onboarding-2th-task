a71b80958ecfa721c18d16c9f1edf77ab6664d72
ovelute53 <ovelute53@naver.com>
2023-08-29 23:26:59 +0900

    feat: api 주소 수정

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.github.com',
+  baseURL: 'https://api.github.com/repos/facebook/react/issues',
 });
 
 if (process.env.NODE_ENV === 'development') {
