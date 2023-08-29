75707f78ebc5b450d2a25042d1d2a575b5623a79
ovelute53 <ovelute53@naver.com>
2023-08-29 22:38:48 +0900

    feat: 광고 이미지 컴포넌트 및 이슈 목록 확장

---
+++ src/components/AdImage/AdImage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AdImage() {
+  return (
+    <a href="https://www.wanted.co.kr/" target="_blank" rel="noopener noreferrer">
+      <img
+        src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100"
+        alt="Wanted"
+      />
+    </a>
+  );
+}
+
+export default AdImage;
