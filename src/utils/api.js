95bc65beb8c505c4ae5a4f5c1bf8458966b7acfe
ovelute53 <ovelute53@naver.com>
2023-08-30 11:56:41 +0900

    octokit install && api.js modified

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,41 @@
-import axios from 'axios';
+import { Octokit } from '@octokit/rest';
 
-const api = axios.create({
+const octokit = new Octokit({
+  auth: import.meta.env.VITE_REACT_APP_OCTOKIT_TOKEN,
   baseURL: 'https://api.github.com/repos/facebook/react/issues',
 });
 
-if (process.env.NODE_ENV === 'development') {
-  api.defaults.headers.common['Authorization'] = `token ${process.env.REACT_APP_GITHUB_TOKEN}`;
-}
+const api = {
+  fetchIssues: async (page = 1) => {
+    try {
+      const response = await octokit.issues.listForRepo({
+        owner: 'facebook',
+        repo: 'react',
+        state: 'open',
+        sort: 'comments',
+        direction: 'desc',
+        per_page: 30, // 예시로 30개의 이슈를 가져옵니다.
+        page: page,
+      });
+
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+  fetchIssueDetail: async issueNumber => {
+    try {
+      const response = await octokit.issues.get({
+        owner: 'facebook',
+        repo: 'react',
+        issue_number: issueNumber,
+      });
+
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
 
 export default api;
