720442231975c4d0615a7a17ca2ba46d735a0788
ovelute53 <ovelute53@naver.com>
2023-08-31 23:54:24 +0900

    feat: react-router-dom v6.15

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import store from '@/redux/store/configureStore.js';
+import store from '@/redux/store/store.js';
+import IssuesList from '@/components/IssuesList/IssuesList.jsx';
+import IssueDetail from '@/components/IssueDetail/IssueDetail.jsx';
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <IssuesList />,
+      },
+      {
+        path: '/:id',
+        element: <IssueDetail />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>,
 );
