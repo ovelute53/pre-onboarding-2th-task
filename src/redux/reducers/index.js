16e8b43674c47545cc855291df1b9bbd8ea2e2b0
ovelute53 <ovelute53@naver.com>
2023-09-01 15:56:31 +0900

    chore: state관리 context API로 변경

--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import issuesReducer from './issuesReducer';
-
-const rootReducer = combineReducers({
-  issues: issuesReducer,
-});
-
-export default rootReducer;
--- src/redux/reducers/issuesReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import issueReducer from '@/redux/modules/issuesSlice';
-
-const rootReducer = combineReducers({
-  issues: issueReducer,
-});
-
-export default rootReducer;
