fe3fa476ae208f23292583a0cd6e472be7eca028
ovelute53 <ovelute53@naver.com>
2023-09-01 17:00:00 +0900

    chore: routing 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from '@/components/Header/Header';
 import IssueList from '@/components/IssuesList/IssuesList';
 import { IssueProvider } from '@/context/IssueContext';
+import IssueDetail from '@/components/IssueDetail/IssueDetail';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<IssueList />} />
+          <Route path="/issue/:issueId" element={<IssueDetail />} />
         </Routes>
       </Router>
     </IssueProvider>
