f9a6aeaca97ff1c92d7d2e6708df91a020535ae4
ovelute53 <ovelute53@naver.com>
2023-09-01 17:27:51 +0900

    style: issueList styling

--- src/components/IssuesList/IssuesList.jsx
+++ src/components/IssuesList/IssuesList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getIssueList } from '@/utils/issueUtils';
 import { useIssues } from '@/context/IssueContext';
 import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
 
 function IssueList() {
   const { issues, setIssues } = useIssues();
@@ -30,13 +31,45 @@ function IssueList() {
   return (
     <div>
       {issues.map(issue => (
-        <div key={issue.id} onClick={() => handleIssueClick(issue.number)}>
-          {issue.title}
-        </div>
+        <IssueContainer key={issue.id} onClick={() => handleIssueClick(issue.number)}>
+          <div>
+            <strong>#{issue.number}</strong> {issue.title}
+          </div>
+          <div>By: {issue.user.login}</div>
+          <div>Date: {new Date(issue.created_at).toLocaleDateString()}</div>
+          <div>Comments: {issue.comments}</div>
+        </IssueContainer>
       ))}
-      <button onClick={() => setPage(prevPage => prevPage + 1)}>Load More</button>
+      <LoadMoreButton onClick={() => setPage(prevPage => prevPage + 1)}>Load More</LoadMoreButton>
     </div>
   );
 }
 
 export default IssueList;
+
+const IssueContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  border: 1px solid #e1e4e8;
+  border-radius: 5px;
+  margin: 10px 0;
+  cursor: pointer;
+  transition: background-color 0.2s;
+  &:hover {
+    background-color: #f6f8fa;
+  }
+`;
+
+const LoadMoreButton = styled.button`
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #0366d6;
+  color: white;
+  cursor: pointer;
+  transition: background-color 0.2s;
+  &:hover {
+    background-color: #0050a0;
+  }
+`;
