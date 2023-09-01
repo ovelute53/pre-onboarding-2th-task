939229eb87320080c9155fd6f68e2a56c403986c
ovelute53 <ovelute53@naver.com>
2023-09-01 17:27:21 +0900

    feat: react-markdown으로 상세페이지 markdown 후, 스타일링

--- src/components/IssueDetail/IssueDetail.jsx
+++ src/components/IssueDetail/IssueDetail.jsx
@@ -1,35 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getIssueDetailItem } from '@/utils/issueUtils';
+import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
 
 function IssueDetail() {
   const [issue, setIssue] = useState(null);
   const { issueId } = useParams();
+  const [markdownContent, setMarkdownContent] = useState('');
+
   useEffect(() => {
     const fetchIssueDetail = async () => {
       try {
         const fetchedIssue = await getIssueDetailItem(issueId);
         setIssue(fetchedIssue);
+        setMarkdownContent(issue.body);
       } catch (error) {
         console.error('Error fetching issue detail:', error);
       }
     };
 
     fetchIssueDetail();
-  }, [issueId]);
+  });
 
   if (!issue) return <div>Loading...</div>;
 
   return (
-    <div>
-      <h1>{issue.title}</h1>
-      <div>
-        <strong>Written by:</strong> {issue.user.login}
-      </div>
-      <img src={issue.user.avatar_url} alt={issue.user.login} width={50} />
-      <p>{issue.body}</p>
-    </div>
+    <IssueWrapper>
+      <IssueTitle>{issue.title}</IssueTitle>
+      <AuthorWrapper>
+        <AuthorImage src={issue.user.avatar_url} alt={issue.user.login} />
+        <AuthorName>Written by: {issue.user.login}</AuthorName>
+      </AuthorWrapper>
+      <IssueContent>
+        <ReactMarkdown>{markdownContent}</ReactMarkdown>
+      </IssueContent>
+    </IssueWrapper>
   );
 }
 
 export default IssueDetail;
+
+const IssueWrapper = styled.div`
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  max-width: 800px;
+  margin: 20px auto;
+`;
+
+const IssueTitle = styled.h1`
+  font-size: 24px;
+  color: #333;
+`;
+
+const AuthorWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+  margin-bottom: 20px;
+`;
+
+const AuthorImage = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 10px;
+`;
+
+const AuthorName = styled.strong`
+  font-size: 16px;
+  color: #555;
+`;
+
+const IssueContent = styled.div`
+  background-color: #f7f7f7;
+  padding: 15px;
+  border-radius: 4px;
+`;
